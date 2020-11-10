7a48736c326f5382d82a225afa116fb54ab74d71
DigitalJay <pushkareva_lisa@mail.ru>
2020-11-10 13:42:41 +0500

    Initial commit

---
+++ george_martin_app/src/main/java/com/example/fun/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.fun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button button1;
+    Button button2;
+    Button button3;
+    TextView total;
+    int counter=0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        total=(TextView) findViewById(R.id.total);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        // по id определеяем кнопку, вызвавшую этот обработчик
+        switch (v.getId()) {
+            case R.id.button1:
+                counter+=1;
+                break;
+            case R.id.button2:
+                counter+=2;
+                break;
+            case R.id.button3:
+                counter+=3;
+                break;
+        }
+        total.setText("Total number of killed today: "+counter);
+    }
+
+
+}
